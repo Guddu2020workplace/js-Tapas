5489a6af3595d8fbfb5ad4989ea186eb418dd2a5
Tapas <mailtotapas92@gamil.com>
2025-02-25 15:17:53 +0530

    DataType conversion

---
+++ 03_DatatypeConversion.js
@@ -0,0 +1,25 @@
+let score = "33abc" //try wth null,undifined,true,only string"Tapas"
+//console.log(typeof score);
+//console.log(typeof (score)); 
+
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn = 1
+//let isLoggedIn = 0
+
+//let isLoggedIn = ""
+//let isLoggedIn = "Tapas"
+
+let somenumber = 33
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+
+//consoleoleanLoggedIn);
