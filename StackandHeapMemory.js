f46168ae9308c0255b0151260b2aa22c60035711
Tapas <mailtotapas92@gamil.com>
2025-02-26 16:25:15 +0530

    Stack memory is associate with primitive data types and heap memory is associated with reffernce datatypes(nonPrimitive)

---
+++ StackandHeapMemory.js
@@ -0,0 +1,20 @@
+// Stack meomry(used in Primitive data types) , Heap Memory (used in Non-Primitive data types)
+
+// Stack Memory:when used ,we get copy of the variable
+// Heap Memory: when used, we get reference of the variable
+
+let myLaptop ="Dell"
+let yourLaptop = myLaptop
+yourLaptop = "HP"
+
+console.log(yourLaptop)
+console.log(myLaptop);
+
+let user = {
+    email: "1@gmail.com",
+    num:007
+}
+let user2 = user
+user2.email = "tap@gmail.com"
+console.log(user.email)
+console.log(user2.email)
