e75c63401dd11eab727917dc6f5749e0f732421d
Tapas <mailtotapas92@gamil.com>
2025-02-26 15:05:24 +0530

    avoid conversion with null,undefined,string with number . when === is used to compare then strict comparision  happens it compares the value and data types also

--- 03_DatatypeConversion.js
+++ 03_DatatypeConversion.js
@@ -16,10 +16,72 @@ let isLoggedIn = 1
 
 let somenumber = 33
 let stringNumber = String(somenumber)
+/*
 console.log(stringNumber);
-console.log(typeof stringNumber);
+console.log(typeof stringNumber);*/
 
 
 let booleanLoggedIn = Boolean(isLoggedIn)
 
 //consoleoleanLoggedIn);
+
+
+// *******************Operations**************** 
+ /*
+let value = 3
+let negValue = -value
+console.log(negValue) */
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%3);
+console.log(2**3);*/
+
+let str1 = "Tapas"
+let str2 = " Pattanaik"
+
+let str3 = str1 + str2
+
+console.log(str3)
+
+/*Logic behind mathematical operation and string operation in javascript
+console.log("1" + 1); //11 => string
+console.log(1+"2"); //12 => string
+console.log(1+2 + "3"); //33 => as first part is mathematical calculation
+so it will add 1+2=3 then it will be string like 3+"3" = 33
+console.log(1+2 + "3" + 4); //334 => as first part is mathematical calculation
+
+*/
+/*
+console.log(true)
+console.log(+true)
+*/
+/*
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)*/
+
+console.log(2>3)
+console.log(2<3)
+console.log(2>=3)
+console.log(2<=3)
+console.log(2==3)
+console.log(2!=3)
+
+console.log(2==="2")
+
+console.log(null>0);
+console.log(null<0);
+console.log(null>=0);
+console.log(null<=0);
+
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined>=0);
+console.log(undefined<=0);
+
+//Never do those abov type of comaprision with null and undefined,string & number
+
+console.log(2==="2")
